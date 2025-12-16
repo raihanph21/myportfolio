14b739c4ffc2ae8e095ba584860e1550fcf3c076
raihanph21 <m.rhn.ph@gmail.com>
2025-12-16 22:49:42 +0700

    wip: navbar layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./index.css";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
+      <Navbar />
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -24,7 +20,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p> */}
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
 function Navbar() {
   return (
-    <nav>
-      <div className="bg-amber-400">
-        <p>Lorem ipsum dolor sit amet.</p>
-      </div>
+    <nav className="w-screen bg-zinc-700 text-white p-5 flex justify-between fixed top-0 font-bold">
+      <ul className="flex gap-6">
+        <li>Raihan</li>
+      </ul>
+      <ul className="flex gap-6 ml-auto me-3">
+        <li>
+          <a href="#">Me</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">Skills</a>
+        </li>
+        <li>
+          <a href="#">Education</a>
+        </li>
+        <li>
+          <a href="#">Resume</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
     </nav>
   );
 }
