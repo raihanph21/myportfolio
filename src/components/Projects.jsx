3eb3af9cafc08e75d16d6c433e0857018aa2b3a1
raihanph21 <m.rhn.ph@gmail.com>
2026-01-12 11:23:51 +0700

    feat: add new projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,7 @@
 import ProjectsCard from "./ProjectsCard";
 import BPAKMUII from "../assets/bpakm.uii.png";
+import splitEase from "../assets/splitEase.png";
+import movieShelf from "../assets/MovieShelf.png";
 
 function Projects() {
   return (
@@ -14,9 +16,19 @@ function Projects() {
           deskripsi="This was a paid group project to rebuild the BPAKM UII website. The project was developed using WordPress by a team of four members, including myself, with each person responsible for building two to three pages. Throughout the process, we ensured that the design and features closely matched the previous website, that the site was fully mobile-responsive, and that all BPAKM UII requirements were met."
         />
         <ProjectsCard
-          img={BPAKMUII}
+          img="sad"
           name="Mini LMS"
           deskripsi="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo architecto numquam dolores beatae aperiam. Modi, cupiditate quasi vitae mollitia nobis magnam rem iste fuga eveniet quae corporis, unde minus ipsa, maxime fugiat voluptates velit reiciendis!"
+        />
+        <ProjectsCard
+          img={splitEase}
+          name="Split Ease"
+          deskripsi="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis fuga quo non ipsa laboriosam eligendi inventore accusantium quidem tenetur officia, dignissimos velit provident dolorum ipsum cumque facere harum? Deleniti quisquam rerum pariatur ex vitae mollitia a ut veritatis fuga voluptas."
+        />
+        <ProjectsCard
+          img={movieShelf}
+          name="Movie Shelf"
+          deskripsi="Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa fugit eius perferendis repellendus iste quos quam omnis vel praesentium magni asperiores pariatur, eos laboriosam nam necessitatibus recusandae accusamus veritatis consequuntur animi porro! Consequatur magni corrupti aliquid sapiente rerum doloribus distinctio, corporis ea tempore, ullam similique odio in voluptatum aliquam provident autem, tenetur minima libero culpa reprehenderit ducimus."
         />
       </div>
     </div>
--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -1,7 +1,7 @@
 function ProjectsCard({ name, deskripsi, img, isiButton, alt }) {
   return (
     <div className="flex flex-col text-center mx-auto mt-5 mb-5 rounded-md border border-white w-xl hover:scale-105 transition-transform duration-300">
-      <img className="rounded-t-xl" src={img} alt={alt} />
+      <img className="rounded-t-md" src={img} alt={alt} />
       <h2 className="font-bold">{name}</h2>
       <p className="text-justify p-2">{deskripsi}</p>
       <button>{isiButton}</button>
