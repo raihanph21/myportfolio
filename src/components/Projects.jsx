321a4771c84ab8fa41c8cfb9781cab7a7887b431
raihanph21 <m.rhn.ph@gmail.com>
2026-01-19 09:43:05 +0700

    feat: Add smooth animation to pop up projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import ProjectsCard from "./ProjectsCard";
 
 function Projects({ projects }) {
@@ -19,31 +20,44 @@ function Projects({ projects }) {
         ))}
       </div>
       {/* pop up  */}
-      {selectedProject && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm">
-          <div className="bg-zinc-800 text-white max-w-lg w-full rounded-md p-6 relative flex flex-col border border-white">
-            <button
-              onClick={() => {
-                setSelectedProject(null);
-              }}
-              className="self-end text-3xl mb-5 hover:cursor-pointer"
+      <AnimatePresence>
+        {selectedProject && (
+          <motion.div
+            className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm p-5"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <motion.div
+              className="bg-zinc-800 text-white max-w-lg w-full rounded-md p-2 lg:p-6 relative flex flex-col border border-white"
+              initial={{ scale: 0.9, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.9, opacity: 0 }}
+              transition={{ duration: 0.25, ease: "easeOut" }}
             >
-              &times;
-            </button>
-            <img src={selectedProject.img}></img>
-            <h2 className="text-center mt-2 mb-1">{selectedProject.name}</h2>
-            <p className="text-justify">{selectedProject.description}</p>
-            <div className="flex justify-between mt-3 *:border *:border-zinc-500 *:p-1 *:rounded-md">
-              <a target="_blank" href={selectedProject.demo}>
-                Demo
-              </a>
-              <a target="_blank" href={selectedProject.github}>
-                Source
-              </a>
-            </div>
-          </div>
-        </div>
-      )}
+              <button
+                onClick={() => {
+                  setSelectedProject(null);
+                }}
+                className="self-end text-3xl mb-5 hover:cursor-pointer"
+              >
+                &times;
+              </button>
+              <img src={selectedProject.img}></img>
+              <h2 className="text-center mt-2 mb-1">{selectedProject.name}</h2>
+              <p className="text-justify">{selectedProject.description}</p>
+              <div className="flex justify-between mt-3 *:border *:border-zinc-500 *:p-1 *:rounded-md">
+                <a target="_blank" href={selectedProject.demo}>
+                  Demo
+                </a>
+                <a target="_blank" href={selectedProject.github}>
+                  Source
+                </a>
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
